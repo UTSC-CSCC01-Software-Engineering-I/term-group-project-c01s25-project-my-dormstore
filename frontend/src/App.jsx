5c63de016a4feba0a081b39d9853cade964ec91d
Judy-Ding <145067157+Judy-Ding@users.noreply.github.com>
2025-06-14 17:30:52 -0400

    Finish the Our Story for Our Story&Blog section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,28 @@ import TopBar from "./components/TopBar";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/Homepage/Homepage"; 
+import OurStory from "./pages/OurStoryBlog/OurStory/OurStory";
+// import Blog from "./pages/OurStoryBlog/Blog/Blog";
+// import Ambassador from "./pages/OurStoryBlog/Ambassador/Ambassador";
 import "./App.css"; // your main styles
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <NavBar />
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+            <TopBar />
+            <Header />
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/our-story" element={<OurStory />} />
+                {/* <Route path="/blog" element={<Blog />} />
+                <Route path="/ambassador" element={<Ambassador />} /> */}
+            </Routes>
+      </div>
+    </Router>
   );
 }
 
