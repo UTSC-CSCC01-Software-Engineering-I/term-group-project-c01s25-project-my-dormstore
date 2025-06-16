5c7c814ab29fcac72728a9dc3acf502ed9bb2628
kouldbebetter <kevinkevin.wang@mail.utoronto.ca>
2025-06-16 00:41:29 -0400

    changed product datatype

--- frontend/src/types/Product.ts
+++ frontend/src/types/Product.ts
@@ -4,4 +4,13 @@ export interface Product {
   price: number;
   image: string;
   description: string;
+  rating: number;
+  variant?: Variant[];
+  included?: string[];
 } 
+
+export interface Variant {
+  id: number
+  label: string    // "Blue Laundry Hamper"
+  value: string    // "blue"
+}
