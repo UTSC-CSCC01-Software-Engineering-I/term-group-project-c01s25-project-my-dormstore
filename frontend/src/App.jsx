e413264bee01a1a6bc6383f9b580a4db6c1bbf14
Ashleylinn <ashleylin1016@gmail.com>
2025-06-24 17:33:29 -0400

    modify navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -178,7 +178,7 @@ function AppContent() {
         </header>
       )}
 
-      {!hidelayout && <NavBar />}
+      {!hidelayout && <NavBar isLoggedIn={isLoggedIn} />}
 
       {showCart ? (
         <CartScreen />
