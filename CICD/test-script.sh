e368f01cb855fb8cc427625ec71ded11e34ae90c
bryntam <112902843+bryntam@users.noreply.github.com>
2025-07-30 16:07:40 -0400

    updated test-script

--- CICD/test-script.sh
+++ CICD/test-script.sh
@@ -6,9 +6,9 @@ sleep 5
 echo "Running health check on http://localhost:5001/api/products"
 
 if curl --fail --silent --show-error http://localhost:5001/api/products; then
-    echo "✅ Server is up and responding!"
+    echo "Server is up and responding!"
     exit 0
 else
-    echo "❌ Server is not responding!"
+    echo "Server is not responding!"
     exit 1
 fi
